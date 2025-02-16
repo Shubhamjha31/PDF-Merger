b40648efa4e9e34cdf85effa859892091a1a24bf
Shubham Jha <jhashubham059@gmail.com>
2025-02-16 18:25:08 +0530

    Some changes

--- server.js
+++ server.js
@@ -18,22 +18,29 @@ const merge = async (file1, file2)=>{
     // Delete the uploaded files after merging.
     fs.unlinkSync(file1);
     fs.unlinkSync(file2);
-}
+};
 
 app.get("/", (req, res) =>{
     res.render("index.ejs");
-})
+    fs.unlink("merged.pdf", (err) => {
+        if (err && err.code !== 'ENOENT') {
+            console.error("Error deleting merged.pdf:", err);
+        } else {
+            console.log("merged.pdf deleted (if existed).");
+        }
+    });
+    
+});
 
 app.get("/about", (req, res) =>{
     res.render("about.ejs");
-})
+});
 
 app.post("/merge", upload.array('pdfs', 2), async function (req, res, next) {
     await merge(req.files[0].path, req.files[1].path);
-    res.download('merged.pdf')
-  })
+    res.download('merged.pdf');
+  });
 
 app.listen(port, ()=>{
     console.log("Server is running on port "+port);
-})
-
+});
